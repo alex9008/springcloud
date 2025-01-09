d5fffd9a3157a412dc22677e96cd42a105763026
alex9008 <aghdez90@gmail.com>
2025-01-09 17:47:25 -0600

    Update configurations in msvc-items.properties, springcloud.code-workspace, pom.xml, and ItemController.java

--- msvc-items/src/main/java/com/alexgarcia/springcloud/msvc/items/controllers/ItemController.java
+++ msvc-items/src/main/java/com/alexgarcia/springcloud/msvc/items/controllers/ItemController.java
@@ -11,28 +11,60 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-
 @RestController
+@RefreshScope
 public class ItemController {
 
     private final ItemService itemService;
     private final CircuitBreakerFactory circuitBreakerFactory;
+    private final Environment env;
+
+    @Value("${configurations.text}")
+    private String text;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${configurations.autor.name}")
+    private String autorName;
+
+    @Value("${configurations.autor.email}")
+    private String autorEmail;
 
     public ItemController(@Qualifier("itemSerciceWebClient") ItemService itemService,
-            CircuitBreakerFactory circuitBreakerFactory) {
+            CircuitBreakerFactory circuitBreakerFactory, Environment env) {
         this.itemService = itemService;
         this.circuitBreakerFactory = circuitBreakerFactory;
+        this.env = env;
     }
+
+    @GetMapping("/fetch-config")
+    public ResponseEntity<?> fetchCofin() {
+
+        Map<String, String> configs = Map.of("text", text, "port", port, "autorName", autorName, "autorEmail",
+                autorEmail);
+        if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+            return ResponseEntity.ok(configs);
+            
+        }
+
+        return ResponseEntity.ok(configs);
+    }
+    
 
     @GetMapping
     public List<Item> list(@RequestParam(name = "name", required = false) String name,
